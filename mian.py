8db92e67e8745e521f63cb88c65a4f43fb401949
Vincent <bohanz0125@gmail.com>
2018-12-10 23:10:06 +0800

    Initial commit

---
+++ mian.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# filename: main.py
+
+import web
+
+urls = (
+    '/wx', 'Handle'
+)
+
+
+class Handle:
+    def get(self):
+        return 'Hello, this is a text msg.'
+
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
