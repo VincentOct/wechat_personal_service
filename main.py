efd1b54a2bb980483e464e5d7abb403b096ff54f
Vincent <bohanz0125@gmail.com>
2018-12-10 23:56:23 +0800

    fix handle.py

--- main.py
+++ main.py
@@ -2,17 +2,11 @@
 # filename: main.py
 
 import web
-# from handle import
+from handle import Handle
 
 urls = (
     '/wx', 'Handle'
 )
-
-
-class Handle:
-    def GET(self):  # 此处的 get 即 http 中的 get 方法。
-        return 'Hello, this is a text msg.'
-
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
